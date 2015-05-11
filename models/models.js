704956c672c6bbbe571bcf7086e8cf03f5e6632a
Ashtryd <raquel.higong@alumnos.upm.es>
2015-05-11 20:37:27 +0200

    Intento Lista preguntas

--- models/models.js
+++ models/models.js
@@ -1,3 +1,4 @@
+var pg= require('pg');
 var path= require('path');
 
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
@@ -26,7 +27,9 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 );
 
 //Importa la definicion de la tabla en quiz.js
-var Quiz= sequelize.import(path.join(__dirname,'quiz'));
+//var Quiz= sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path=path.join(__dirname,'quiz');
+var Quiz= sequelize.import(quiz_path);
 
 exports.Quiz= Quiz; //Exporta la definicion en Quiz
 
@@ -38,10 +41,10 @@ sequelize.sync().then(function(){
   Quiz.count().then(function(count){
     if(count === 0){//ls tabla se inicializa solo si esta vacia
       //Crea primera pregunta
-      		Quiz.create({pregunta: '¿Capital de Italia?',
-      		respuesta: 'Roma'
-      })
-      .then(function(){console.log('Base de Datos inicializada')});
+      	Quiz.bulkCreate(
+        [ {pregunta: 'Capital de Italia', respuesta: 'Roma'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}]
+      ).then(function(){console.log('BBDD inicializada')});	
     };
   });
 });
